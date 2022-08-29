faefa1a97143a172739332106bec8d6435f58634
Owen <77976026+Owen7000@users.noreply.github.com>
2022-08-29 13:15:27 +0100

    commit

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 let colour = '#3aa757'
 
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ colour });
     console.log('Default background colour set to %cgreen', `color: ${colour}`);
